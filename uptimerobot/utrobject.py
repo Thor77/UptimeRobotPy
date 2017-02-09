28651041c58a0b8a93ad438d500b9229ee9a5101
Thor77 <thor77@thor77.org>
2017-02-09 20:04:50 +0100

    Use __getattr__ instead of __getattribute__
    for UptimeRobot-specific attributes (stored in .attributes)
    instead of an own implementation of the same basic thing

--- uptimerobot/utrobject.py
+++ uptimerobot/utrobject.py
@@ -3,11 +3,8 @@ class UptimeRobotObject(object):
         self.account = account
         self.attributes = attributes
 
-    def __getattribute__(self, name):
-        if name in super().__getattribute__('attributes'):
-            return super().__getattribute__('attributes')[name]
-        else:
-            return super().__getattribute__(name)
+    def __getattr__(self, name):
+        return self.attributes[name]
 
     def __repr__(self):
         return '{0.friendly_name} ({0.id}) <{0.status}>'.format(self)
